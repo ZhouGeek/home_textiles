62259c86e027d50b86dd37f80eb9c3503329f3ff
zhourenzhi <zhourenzhi@tele-hot.com>
2019-01-15 14:42:39 +0800

    集成logback日志 修改commons-fileupload version 1.3.3

--- core/src/main/java/com/geekz/core/system/controller/UserController.java
+++ core/src/main/java/com/geekz/core/system/controller/UserController.java
@@ -6,6 +6,8 @@ import com.geekz.core.constants.BaseEnums;
 import com.geekz.core.system.dto.User;
 import com.geekz.core.system.service.UserService;
 import com.geekz.core.util.Results;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +24,16 @@ import java.util.List;
 @RestController
 public class UserController extends BaseController {
 
+    /**
+     * 可以看到引入的包是slf4j.Logger，代码里并没有引用任何一个跟 Logback 相关的类，这便是使用 Slf4j的好处，在需要将日志框架切换为其它日志框架时，无需改动已有的代码。
+     * LoggerFactory 的 getLogger() 方法接收一个参数，以这个参数决定 logger 的名字，比如第二图中的日志输出。在为 logger 命名时，用类的全限定类名作为 logger name 是最好的策略，这样能够追踪到每一条日志消息的来源
+     * 可以看到，可以通过提供占位符，以参数化的方式打印日志，避免字符串拼接的不必要损耗，也无需通过logger.isDebugEnabled()这种方式判断是否需要打印。
+     */
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    /**
+     * userService
+     */
     @Autowired
     private UserService userService;
 
@@ -35,6 +47,11 @@ public class UserController extends BaseController {
     @RequestMapping("/sys/user/queryOne/{userId}")
     public Result queryOne(@PathVariable Long userId){
         User user = userService.get(userId);
+
+        logger.debug("userId:{},userName:{},birthday:{}", user.getUserId(), user.getUsername(), user.getBirthday());
+        logger.info("userId:{},userName:{},birthday:{}", user.getUserId(), user.getUsername(), user.getBirthday());
+        logger.error("userId:{},userName:{},birthday:{}", user.getUserId(), user.getUsername(), user.getBirthday());
+
         return Results.successWithData(user);
     }
 
